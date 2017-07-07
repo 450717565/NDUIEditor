74bf3e9e946f578d5ed34cae3e1d5cd3cb034017
VIKING_YFY <450717565@qq.com>
2017-07-07 14:09:59 +0800

    尝试优化鼠标中键战复/复活功能

--- Interface/AddOns/NDui/Libs/oUF/Plugins/AutoResurrect.lua
+++ Interface/AddOns/NDui/Libs/oUF/Plugins/AutoResurrect.lua
@@ -8,51 +8,56 @@ local oUF = ns.oUF
 
 local classList = {
 	["DEATHKNIGHT"] = {
-		combat = GetSpellInfo(61999),	-- Raise Ally
+		combat = GetSpellInfo(61999),		-- 复活盟友
 	},
 	["DRUID"] = {
-		combat = GetSpellInfo(20484),	-- Rebirth
-		ooc = GetSpellInfo(50769),		-- Revive
+		combat = GetSpellInfo(20484),		-- 复生
+		oneres = GetSpellInfo(50769),		-- 起死回生
+		allres = GetSpellInfo(212040),		-- 新生
 	},
 	["MONK"] = {
-		ooc = GetSpellInfo(115178),		-- Resuscitate
+		oneres = GetSpellInfo(115178),		-- 轮回转世
+		allres = GetSpellInfo(212051),		-- 死而复生
 	},
 	["PALADIN"] = {
-		ooc = GetSpellInfo(7328),		-- Redemption
+		oneres = GetSpellInfo(7328),		-- 救赎
+		allres = GetSpellInfo(212056),		-- 宽恕
 	},
 	["PRIEST"] = {
-		ooc = GetSpellInfo(2006),		-- Resurrection
+		oneres = GetSpellInfo(2006),		-- 复活术
+		allres = GetSpellInfo(212036),		-- 群体复活
 	},
 	["SHAMAN"] = {
-		ooc = GetSpellInfo(2008),		-- Ancestral Spirit
+		oneres = GetSpellInfo(2008),		-- 先祖之魂
+		allres = GetSpellInfo(212048),		-- 先祖视界
 	},
-	["HUNTER"] = {},
-	["WARLOCK"] = {},
+	["HUNTER"] = {
+		combat1 = GetSpellInfo(126393),		-- 永恒守护者
+		combat2 = GetSpellInfo(159931),		-- 赤精之赐
+	},
+	["WARLOCK"] = {
+		combat = GetSpellInfo(20707),		-- 灵魂石
+	},
 }
 
 local body = ""
 local function macroBody(class)
-	body = "/stopmacro [@mouseover,nodead]\n"
+	body = "/stopcasting\n"
 
 	if class == "HUNTER" then
-		local name1 = GetSpellInfo(126393)	--魁麟
-		body = body.."/cast [@mouseover,help,dead] "..name1.."\n"
-		local name2 = GetSpellInfo(159931)	--鹤
-		body = body.."/cast [@mouseover,help,dead] "..name2.."; "
-	elseif class == "WARLOCK" then
-		local name = GetSpellInfo(20707)	--灵魂石
-		body = body.."/cast [@mouseover,help,dead] "..name.."; "
+		local combatSpell1 = classList[class].combat1
+		local combatSpell2 = classList[class].combat2
+		body = body.."/cast [combat,help,dead] "..combatSpell1.."; [combat,@mouseover,help,dead] "..combatSpell1.."\n"
+		body = body.."/cast [combat,help,dead] "..combatSpell2.."; [combat,@mouseover,help,dead] "..combatSpell2.."\n"
 	else
-		local combatspell = classList[class].combat
-		local oocspell = classList[class].ooc
-		if combatspell then
-			body = body.."/cast [combat,@mouseover,help,dead] "..combatspell.."; "
-
-			if oocspell then
-				body = body.."[@mouseover,help,dead] "..oocspell.."; "
-			end
-		elseif oocspell then
-			body = body.."/cast [@mouseover,help,dead] "..oocspell.."; "
+		local combatSpell = classList[class].combat
+		local oneresSpell = classList[class].oneres
+		local allresSpell = classList[class].allres
+		if combatSpell then
+			body = body.."/cast [combat,help,dead] "..combatSpell.."; [combat,@mouseover,help,dead] "..combatSpell.."\n"
+		else
+			body = body.."/cast [nocombat] "..allresSpell.."\n"
+			body = body.."/cast [nocombat,help,dead] "..oneresSpell.."; [combat,@mouseover,help,dead] "..oneresSpell.."\n"
 		end
 	end
 
